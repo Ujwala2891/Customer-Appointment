7e897712a4d64d22a8a56dcdbe5ae1714bd1c3aa
Ujwala2891 <35549517+Ujwala2891@users.noreply.github.com>
2018-03-08 12:33:14 -0600

    Add files via upload

--- Hostpital/src/main/webapp/resources/theme1/JS/script1.js
+++ Hostpital/src/main/webapp/resources/theme1/JS/script1.js
@@ -5,38 +5,34 @@ var app = angular.module('appoinmentApp', []);
 
 /* Getting Data from APi through  Ajax /Http */
 
+  app.controller('appoinmentctrl', function($scope, $http) {
+			   
+	 
+	   $http({
+	        method : "GET",
+	        url : '/Hostpital/appointment/json/fulldata'
+	    }).then(function mySuccess(response) {
+	        $scope.appList = response.data;
+	    }, function myError(response) {
+	        $scope.appList = response.statusText;
+	    });
+	 
+	  
+	 $scope.getAppointments= function(searchText){
+		  
 	
-	 app.controller('appoinmentctrl', function($scope, $http) {
-		
-		
-		  $http({
-		    method : "GET",
-		    url : "/Hostpital/appointment/json/fulldata"
-		    	
-		  }).then(function mySuccess(response) {
-			 
-		      $scope.appList = response.data;
-		      alert(applist);
-		    }, function myError(response) {
-		      $scope.myWelcome = response.statusText;
-		  });
-	
-
-
-
-
-
-$scope.getAppointments = function(searchText){
-	
-	$scope.showSearchResults = true;
+	$scope.showResultTable = true;
 	$scope.temptext = searchText;
-	$scope.showAllRows = false;
+	/*$scope.showAllRows = false;*/
 		
 }
+	  
 
 
 
 }); 
+  
+
 
 
 
